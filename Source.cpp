882bc90efc43ad91ce322ade25b052f573744825
konkenman <goodyshoes_nt@yahoo.com>
2015-06-08 09:00:28 -0700

    Update Source.cpp

--- Source.cpp
+++ Source.cpp
@@ -1,22 +1,72 @@
 #include <iostream>
 #include <string>
+#include <fstream>
 
 using namespace std;
 
+bool greetUser();
 void mainMenu();
+bool openInputFile(ifstream &ifs);
 
 int main()
 {
-
+  int choice = 0;
   
+  if (! greetUser())
+    return 0;
+  
+  mainMenu();
+  cout << "Enter your choice: ";
+  cin >> choice;
+  while (choice != 5)
+  {
+  switch (choice)
+  {
+    case 5:
+    {
+      cout << endl;
+      cout << "Thanks for using this program." << endl;
+      return 0;
+    }
+     
+    default:
+      cout << "You have entered an invalid number!" << endl;
+      break;
+  }
+  }
   return 0;
 }
 
+bool greetUser()
+{
+  char choice;
+  cout << "Welcome to the Euler's Path Program!" << endl;
+  cout << endl;
+  cout << "Do you want to run this program? " << endl;
+  
+  cout << "Type Y for YES or N for NO: ";
+  cin >> choice;
+  
+  if (choice == 'Y')
+    return true;
+  return false;
+}
 void mainMenu()
 {
-  cout << "1. ";
-  cout << "2. Add or remove graph edges" << endl;
-  cout << "3. ";
-  cout << "4. ";
+  cout << "1. Add a graph edge" << endl;
+  cout << "2. Remove a graph edge" << endl;
+  cout << "3. Solve the graph (Euler's Path)" << endl;
+  cout << "4. Write graph to file" << endl;
+  cout << "5. Quit the program" << endl;
+}
+
+bool openInputFile(ifstream &ifs)
+{
+	string filename;
+
+	cout << "Enter the input filename: ";
+	getline(cin, filename);
+	ifs.open(filename.c_str());
+	return ifs.is_open();
 }
 
